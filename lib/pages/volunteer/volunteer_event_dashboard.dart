5b9e3341f1f2de7e05a461a54029fce74e20dfa1
alyssaongyx <ongyixian@gmail.com>
2024-02-09 02:24:49 +0800

    Fetch profile picture from firebase

--- lib/pages/volunteer/volunteer_event_dashboard.dart
+++ lib/pages/volunteer/volunteer_event_dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hackforgood24/pages/admin/bottom_navigation_bar.dart';
 
 class VolunteerEventDashboard extends StatefulWidget {
@@ -149,7 +150,26 @@ class _AnimatedProgressCardState extends State<AnimatedProgressCard>
               style: Theme.of(context).textTheme.headline6,
             ),
             SizedBox(height: 8.0),
-            Image.asset('assets/images/monster_avatar.png'),
+            StreamBuilder<DocumentSnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('users')
+                  .doc(FirebaseAuth.instance.currentUser!.uid)
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+                var profilePictureURL =
+                    snapshot.data!['profilePictureURL'] ?? '';
+                return CircleAvatar(
+                  radius: 40,
+                  backgroundImage: profilePictureURL.isNotEmpty
+                      ? NetworkImage(profilePictureURL)
+                      : AssetImage('assets/images/default_profile.png')
+                          as ImageProvider,
+                );
+              },
+            ),
             SizedBox(height: 8.0),
             Text(
               'Hours left to defeat ${hoursLeft.toStringAsFixed(1)}',
